746e271b5c327f2bd65001f3c9d13a871027f188
Nitish Kumar <nitish7290951173@gmail.com>
2026-06-21 23:34:01 +0530

    Update blog.js

--- js/blogs.js
+++ js/blogs.js
@@ -1,28 +1,33 @@
-fetch("data/blogs.json")
-  .then(res => res.json())
-  .then(blogs => {
-    const container = document.getElementById("blogContainer");
+const container = document.getElementById("blogsContainer");
 
-    blogs.forEach(blog => {
-      const article = document.createElement("article");
-      article.className = "blog-card";
-      article.innerHTML = `
-        <h3>${blog.title}</h3>
-        <small>${blog.author} • ${blog.date}</small>
-        <div class="blog-content">
-          ${renderMarkdown(blog.content)}
-        </div>
-      `;
-      container.appendChild(article);
+fetch("assets/data/blogs.json")
+.then(res=>res.json())
+.then(data=>{
+
+    data.reverse().forEach(blog=>{
+
+        const card=document.createElement("div");
+
+        card.className="blog-card";
+
+        card.innerHTML=`
+
+        <img src="${blog.image}">
+
+        <h2>${blog.title}</h2>
+
+        <p>${blog.date}</p>
+
+        <button onclick="window.location.href='blog-view.html?id=${blog.id}'">
+
+        Read More
+
+        </button>
+
+        `;
+
+        container.appendChild(card);
+
     });
-  });
 
-function renderMarkdown(text) {
-  return text
-    .replace(/^### (.*$)/gim, "<h4>$1</h4>")
-    .replace(/^## (.*$)/gim, "<h3>$1</h3>")
-    .replace(/^# (.*$)/gim, "<h2>$1</h2>")
-    .replace(/\*\*(.*?)\*\*/gim, "<strong>$1</strong>")
-    .replace(/```([\s\S]*?)```/gim, "<pre><code>$1</code></pre>")
-    .replace(/\n/gim, "<br>");
-}
+});
